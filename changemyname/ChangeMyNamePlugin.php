a17566e3c2b35dab142cdae50c8d6ba5b139e9f3
Jason McCallister <themccallister@gmail.com>
2014-04-08 14:13:45 -0700

    Initial Commit

---
+++ changemyname/ChangeMyNamePlugin.php
@@ -0,0 +1,107 @@
+<?php
+namespace Craft;
+
+/**
+ * Change My Name @v1.0
+ *
+ * Example plugin that demonstrates how to use an alias to change
+ * a plugins name and provide the option to display the plugins
+ * cp section in Craft - inspired by plugins by @selvinortiz
+ *
+ * @author		Jason McCallister - http://themccallister.com
+ * @package		Change My Name
+ * @copyright	2014 Jason McCallister
+ * @license		[MIT]
+ *
+ */
+
+class ChangeMyNamePlugin extends BasePlugin
+{
+
+    /**
+     * Returns the plugins name or alias
+     *
+     * @param bool $real
+     * @return mixed|string
+     */
+    public function getName($real=false)
+    {
+        $name	= 'Change My Name';
+        $alias	= $this->getSettings()->pluginAlias;
+
+        if ($real)
+        {
+            return $name;
+        }
+
+        return empty($alias) ? $name : $alias;
+    }
+
+    /**
+     * Returns the plugin’s version.
+     *
+     * @return string
+     */
+    public function getVersion()
+    {
+        return '1.0';
+    }
+
+    /**
+     * Returns the plugin developer's name.
+     *
+     * @return string
+     */
+    public function getDeveloper()
+    {
+        return 'Jason McCallister';
+    }
+
+    /**
+     * Returns the plugin developer's URL.
+     *
+     * @return string
+     */
+    public function getDeveloperUrl()
+    {
+        return 'http://themccallister.com';
+    }
+
+    /**
+     * Returns whether or not the plugin has its own cp section
+     *
+     * @return mixed
+     */
+    public function hasCpSection()
+    {
+        return $this->getSettings()->enableCpTab;
+    }
+
+    /**
+     * Returns array of the plugins settings.
+     * Used in changemyname/templates/_settings
+     *
+     * @return array
+     */
+    public function defineSettings()
+    {
+        $url = craft()->getSiteUrl();
+
+        return array(
+            'enableCpTab'	=> array(AttributeType::Bool, 'default' => true),
+            'pluginAlias'	=> AttributeType::String
+        );
+    }
+
+    /**
+     * @return string
+     * @throws \LogicException
+     * @throws \Twig_Error_Runtime
+     */
+    public function getSettingsHtml()
+    {
+        $settings = $this->getSettings();
+
+        return craft()->templates->render('changemyname/_settings', compact('settings'));
+    }
+}
---
+++ changemyname/variables/ChangeMyNameVariable.php
@@ -0,0 +1,77 @@
+<?php
+namespace Craft;
+
+/**
+ * Change My Name @v1.0
+ *
+ * Example plugin that demonstrates how to use an alias to change
+ * a plugins name and provide the option to display the plugins
+ * cp section in Craft - inspired by plugins by @selvinortiz
+ *
+ * @author		Jason McCallister - http://themccallister.com
+ * @package		Change My Name
+ * @copyright	2014 Jason McCallister
+ * @license		[MIT]
+ *
+ */
+class ChangeMyNameVariable
+{
+    protected $plugin;
+
+    public function __construct()
+    {
+        $this->plugin = craft()->plugins->getPlugin('changemyname');
+    }
+
+    /**
+     * Returns getName for use in templates
+     *
+     * @param bool $real
+     * @return string
+     */
+    public function getName($real=true)
+    {
+        return $this->plugin->getName($real);
+    }
+
+    /**
+     * Returns getVersion for use in templates
+     *
+     * @return string
+     */
+    public function getVersion()
+    {
+        return $this->plugin->getVersion();
+    }
+
+    /**
+     * Returns getDeveloper for use in templates
+     *
+     * @return string
+     */
+    public function getDeveloper()
+    {
+        return $this->plugin->getDeveloper();
+    }
+
+    /**
+     * Returns getDeveloperUrl for use in templates
+     *
+     * @return string
+     */
+    public function getDeveloperUrl()
+    {
+        return $this->plugin->getDeveloperUrl();
+    }
+
+    /**
+     * Returns getUrl for the plugin for use in templates
+     *
+     * @return string
+     */
+    public function getUrl()
+    {
+        return sprintf('/%s/changemyname', craft()->config->get('cpTrigger'));
+    }
+
+}
